4453d3c39e4d43f7ea79d85a1fef07fed20e4094
synrisea <150649087+synrisea@users.noreply.github.com>
2025-02-16 16:28:35 +0400

    Implement task item display with edit, delete, and toggle functionality

---
+++ taskboard/src/features/tasks/TaskList/TaskItem/TaskItem.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { deleteTask, toggleTask } from '../../../tasksSlice';
+import '../TaskList.css'
+
+const TaskItem = ({ task }) => {
+    const dispatch = useDispatch();
+    const location = useLocation();
+    const navigate = useNavigate();
+
+    const isActive = location.pathname === `/${task.id}` || location.pathname === `/${task.id}/edit`;
+
+    const handleDelete = () => {
+        dispatch(deleteTask(task.id));
+        if (isActive) navigate('/');
+    };
+
+    return (
+        <li>
+            <div className={`task-item-container ${isActive ? 'task-item-active' : ''}`}>
+                <div>
+                    <input
+                        type="checkbox"
+                        checked={task.completed}
+                        onChange={() => dispatch(toggleTask(task.id))}
+                    />
+                    <Link to={`/${task.id}`} className="task-link">{task.title}</Link>
+                </div>
+                <div>
+                    <button className="edit-button" onClick={() => navigate(`/${task.id}/edit`)}>Redact</button>
+                    <button onClick={handleDelete} className="button button-red">Delete</button>
+                </div>
+            </div>
+        </li>
+    );
+};
+
+export default TaskItem;
