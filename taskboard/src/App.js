0423da4ebac2f376e4fed8b414f430c54461b808
synrisea <150649087+synrisea@users.noreply.github.com>
2025-02-16 14:12:34 +0400

    Implemented routing in Task Manager app. Added routes for the main page, task details, and task editing. Added a fallback route that redirects to the home page for any invalid paths.

--- taskboard/src/App.js
+++ taskboard/src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import Layout from './Layout';
+import TaskDetails from './features/tasks/TaskDetails/TaskDetails';
+import EditTask from './features/tasks/EditTask/EditTask';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<h1>Welcome to the Task Manager!</h1>} />
+        <Route path=":taskId" element={<TaskDetails />} />
+        <Route path=":taskId/edit" element={<EditTask />} />
+      </Route>
+      <Route path="*" element={<Navigate to="/" />} />
+    </Routes>
   );
 }
 
